249e5160ffd1757279e27b839ac3e6ce97a67a31
Ali Almasi <a710almasi@gmail.com>
2025-10-11 00:35:27 +0330

    fix: button in button error

--- components/blocks/Navigation.tsx
+++ components/blocks/Navigation.tsx
@@ -1,41 +1,37 @@
 "use client";
 
 import { Button } from "../ui/button";
-import Tip from "../Tip";
 import newTab from "@/lib/newTab";
 import { ModeToggle } from "../ModeToggle";
-import { GithubLogoIcon } from "@phosphor-icons/react/dist/ssr";
-import { TwitterLogoIcon } from "@phosphor-icons/react/dist/ssr";
-import { BookOpen } from "lucide-react";
+import {
+  GithubLogoIcon,
+  TwitterLogoIcon,
+} from "@phosphor-icons/react/dist/ssr";
+import { BookOpen, Download, Languages } from "lucide-react";
 
 export default function Navigation() {
   return (
     <nav className="flex flex-row-reverse items-center gap-2">
-      <Tip tipText="Switch theme">
-        <ModeToggle />
-      </Tip>
-      <Tip tipText="GitHub">
-        <Button
-          variant="outline"
-          size="icon"
-          onClick={() => newTab("https://github.com/localshare")}
-        >
-          <GithubLogoIcon weight="bold" />
-        </Button>
-      </Tip>
-      <Tip tipText="Twitter">
-        <Button variant="outline" size="icon" onClick={() => null}>
-          <TwitterLogoIcon weight="bold" />
-        </Button>
-      </Tip>
-      <Tip tipText="Documentation">
-        <Button variant="outline" size="icon" onClick={() => null}>
-          <BookOpen />
-        </Button>
-      </Tip>
-      <Tip tipText="Coming soon">
-        <Button>Download</Button>
-      </Tip>
+      <ModeToggle />
+      <Button variant="outline" size="icon" onClick={() => null}>
+        <Languages />
+      </Button>
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => newTab("https://github.com/localshare")}
+      >
+        <GithubLogoIcon weight="bold" />
+      </Button>
+      <Button variant="outline" size="icon" onClick={() => null}>
+        <TwitterLogoIcon weight="bold" />
+      </Button>
+      <Button variant="outline" size="icon" onClick={() => null}>
+        <BookOpen />
+      </Button>
+      <Button title="Coming Soon" className="hidden md:flex">
+        Download <Download />
+      </Button>
     </nav>
   );
 }
