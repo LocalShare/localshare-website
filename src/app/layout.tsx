25f4bbf9e5697b824270029693cd8362731d6ef4
Ali Almasi <a710almasi@gmail.com>
2025-10-11 00:10:53 +0330

    fix: changed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 
 import { ThemeProvider } from "@/components/theme-provider";
 
 import "./globals.css";
 
-const sfpro = localFont({
-  src: "./SF-Pro.ttf",
-  display: "swap",
-  style: "normal",
-  weight: "100 900",
-});
+const inter = Inter();
 
 export const metadata: Metadata = {
   title: "LocalShare",
@@ -27,7 +22,7 @@ export default function RootLayout({
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
-        <body className={`${sfpro.className} antialiased select-none`}>
+        <body className={`${inter.className} antialiased select-none`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
